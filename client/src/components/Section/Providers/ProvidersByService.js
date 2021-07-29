274804cf643337170e4a1b37fd0045bf08f10cc2
eugebutta <eugebutta90@gmail.com>
2021-07-29 12:13:44 -0300

    Buttom order rating

--- client/src/components/Section/Providers/ProvidersByService.js
+++ client/src/components/Section/Providers/ProvidersByService.js
@@ -4,12 +4,19 @@ import { getProvidersbyServiceName } from "../../../Redux/actions/actions";
 import { useParams } from "react-router-dom";
 import Provider from "./Provider/Provider.js";
 import "./ProvidersByService.scss";
+import { BsArrowUpShort, BsArrowDownShort } from 'react-icons/bs'
 
 export function ProvidersByService() {
   const dispatch = useDispatch();
   const providers = useSelector((state) => state.providersByService.data);
 
+  console.log(providers?.map(x => x.rating))
+
   const { serviceName } = useParams();
+
+/*  const  masRating = () => { 
+   
+ } */
 
   useEffect(() => {
     dispatch(getProvidersbyServiceName(serviceName));
@@ -25,6 +32,10 @@ export function ProvidersByService() {
               <h1 className="title">{`${serviceName}`}</h1>
               <h4 className='title'>Prestadores disponibles</h4>
             </div>
+          </div>
+          <div style={{display:"flex" , marginTop:"1rem"}}>
+          <button style={{display:"flex", alignItems:"center"}} onClick={()=> masRating ()} class="sort-asc"><BsArrowUpShort /> Mas Reating </button>
+          <button /* onClick={()=> menosRating ()} */style={{display:"flex", alignItems:"center", marginLeft:"1rem"}} class="sort-asc"> <BsArrowDownShort/> Menos Reating</button>
           </div>
           <div className="providers-list">
             {providers &&
