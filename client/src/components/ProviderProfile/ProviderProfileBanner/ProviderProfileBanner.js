ad0dca17c3d9a1e67a705c2ac60d8858bdcb3e47
Dabuaf86 <dabuaf@gmail.com>
2021-07-29 19:09:57 -0300

    last changes merged

--- client/src/components/ProviderProfile/ProviderProfileBanner/ProviderProfileBanner.js
+++ client/src/components/ProviderProfile/ProviderProfileBanner/ProviderProfileBanner.js
@@ -5,11 +5,11 @@ import Image from '../../../img/banner.jpg';
 function ProviderProfileBanner({ classes, provider }) {
     return (
         <Grid item className={classes.gridBanner}>
-            {/* <Box className={classes.containerBanner}>
+            <Box className={classes.containerBanner}>
                 <img className={classes.bannerImg} src={Image}/>
                 <Typography variant='h4' className={classes.bannerText}>¡Hola {provider.firstName}!</Typography>
                 <Typography variant='subtitle1' className={classes.bannerTextSubt}>En este espacio vas a poder ver y editar tu perfil.</Typography>
-            </Box> */}
+            </Box>
         </Grid>
     )
 };
